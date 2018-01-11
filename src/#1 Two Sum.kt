49b89bda5c194bd947fb50a70022f98591193b6a
九条涼果 <s1131234@gmail.com>
2018-01-11 16:33:23 +0800

    1. Two Sum https://leetcode.com/problems/two-sum

--- src/#1 Two Sum.kt
+++ src/#1 Two Sum.kt
@@ -3,6 +3,17 @@ import io.kotlintest.specs.StringSpec
 
 class Solution1 {
     fun twoSum(nums: IntArray, target: Int): IntArray {
+        val cache = mutableMapOf<Int,Int>()
+        nums.forEachIndexed { index, i ->
+            val wish = target - i
+            if (cache.contains(wish))
+                return intArrayOf(index, cache[wish]!!)
+            cache[i] = index
+        }
+
+        return intArrayOf(-1, -1)
+    }
+    fun twoSum2(nums: IntArray, target: Int): IntArray {
         for ((index, value) in nums.withIndex()) {
             val wish = target - value
             val i = nums.indexOf(wish)
@@ -11,13 +22,6 @@ class Solution1 {
             }
         }
 
-//        nums.forEachIndexed { index, i ->  nums.drop(index).find { target - i == it} }
-//        val associateBy = nums.mapIndexed { index, i -> index to i }.sortedBy { it.second }
-//        associateBy.forEach { first ->
-//            val search = associateBy.binarySearch { target - it.second }
-//            if (search == first.second)
-//                return intArrayOf(first.first, search)
-//        }
         return intArrayOf(-1, -1)
     }
 }
@@ -26,16 +30,16 @@ class Solution1Test : StringSpec() {
     init {
         Solution1().run {
             "[2, 7, 11, 15], 9" {
-                twoSum(intArrayOf(2, 7, 11, 15), 9) shouldBe intArrayOf(0, 1)
+                twoSum(intArrayOf(2, 7, 11, 15), 9).sortedArray() shouldBe intArrayOf(0, 1)
             }
             "[3, 2, 4], 6"{
-                twoSum(intArrayOf(3, 2, 4), 6) shouldBe intArrayOf(1, 2)
+                twoSum(intArrayOf(3, 2, 4), 6).sortedArray() shouldBe intArrayOf(1, 2)
             }
             "[3, 2, 4, 5], 6"{
-                twoSum(intArrayOf(3, 2, 4, 5), 6) shouldBe intArrayOf(1, 2)
+                twoSum(intArrayOf(3, 2, 4, 5), 6).sortedArray() shouldBe intArrayOf(1, 2)
             }
             "[3, 2, 5, 4, 5], 6"{
-                twoSum(intArrayOf(3, 2, 5, 4, 5), 6) shouldBe intArrayOf(1, 3)
+                twoSum(intArrayOf(3, 2, 5, 4, 5), 6).sortedArray() shouldBe intArrayOf(1, 3)
             }
         }
     }
