c44e78fae48d2428549162521ffafcd138eb088e
Ryoka Kujo <s1131234@gmail.com>
2020-09-14 23:11:16 +0800

    ✨ 94. Binary Tree Inorder Traversal

---
+++ solution/#94 Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,85 @@
+package leetcode.q94.java;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.stream.Stream;
+
+import leetcode.base.java.JavaTest;
+import org.junit.jupiter.params.provider.Arguments;
+
+/**
+ * <a href="https://leetcode-cn.com/problems/binary-tree-inorder-traversal/">
+ * 94. Binary Tree Inorder Traversal
+ * </a>
+ */
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        dfs(answer, new ArrayDeque<>(), root);
+        return answer;
+    }
+
+    private static void dfs(List<Integer> answer, Deque<Integer> stack, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        stack.push(root.val);
+        dfs(answer, stack, root.left);
+        answer.add(stack.pop());
+        dfs(answer, stack, root.right);
+    }
+
+    public List<Integer> inorderTraversa2(TreeNode root) {
+        List<Integer>   answer = new ArrayList<>();
+        Deque<TreeNode> stack  = new ArrayDeque<>();
+
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            answer.add(node.val);
+            node = node.right;
+        }
+        return answer;
+    }
+
+
+    static class TreeNode {
+
+        int      val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    static class SolutionTest extends JavaTest<Solution> {
+
+        @Override
+        protected Stream<Arguments> provider() {
+            return Stream.of(
+                Arguments.of(node(1, null, node(2, node(3), null)),
+                             lists(1, 3, 2)),
+                Arguments.of(node(1, node(4, node(5), null), node(2, node(3), null)),
+                             lists(5, 4, 1, 3, 2))
+            );
+        }
+
+        private static TreeNode node(int value) {
+            return new TreeNode(value);
+        }
+
+        private static TreeNode node(int value, TreeNode left, TreeNode right) {
+            TreeNode node = new TreeNode(value);
+            node.left  = left;
+            node.right = right;
+            return node;
+        }
+    }
+}
