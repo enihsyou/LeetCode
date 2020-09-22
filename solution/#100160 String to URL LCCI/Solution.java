f5a911c5a80d74927f6cbd64a1b5e9a1dd431cf8
Ryoka Kujo <s1131234@gmail.com>
2020-09-22 14:17:11 +0800

    ✨ 面试题 01.03. URL化

--- solution/#100160 String to URL LCCI/Solution.java
+++ solution/#100160 String to URL LCCI/Solution.java
@@ -13,20 +13,20 @@ import org.junit.jupiter.params.provider.Arguments;
 class Solution {
 
     public String replaceSpaces(String S, int length) {
-        char[] content = new char[S.length()];
+        char[] content = S.toCharArray();
 
-        int p = 0;
-        for (int i = 0; i < length; i++) {
+        int p = S.length();
+        for (int i = length - 1; i >= 0; i--) {
             if (S.charAt(i) == ' ') {
-                content[p++] = '%';
-                content[p++] = '2';
-                content[p++] = '0';
+                content[--p] = '0';
+                content[--p] = '2';
+                content[--p] = '%';
             } else {
-                content[p++] = S.charAt(i);
+                content[--p] = S.charAt(i);
             }
         }
 
-        return new String(content, 0, p);
+        return new String(content, p, S.length() - p);
     }
 
     static class SolutionTest extends JavaTest<Solution> {
@@ -34,6 +34,7 @@ class Solution {
         @Override
         protected Stream<Arguments> provider() {
             return Stream.of(
+                Arguments.of("   ", 1, "%20"),
                 Arguments.of("Mr John Smith    ", 13, "Mr%20John%20Smith"),
                 Arguments.of("               ", 5, "%20%20%20%20%20"),
                 Arguments.of("ds sdfs afs sdfa dfssf asdf             ", 27, "ds%20sdfs%20afs%20sdfa%20dfssf%20asdf")
